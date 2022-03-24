db0021314c78502196363465db08a14f25cd19e1
sgraciamyriad <sgracia@myriad.com>
2022-03-24 16:29:26 -0500

    change to cloudformation command in shell script.

--- createstack.sh
+++ createstack.sh
@@ -1,13 +1,13 @@
+#!/bin/bash
+
+
 echo "Creating stack..."
 
 STACK_NAME="myriadbaseinfra"
 STACK_ID=$( \
   aws cloudformation create-stack \
   --stack-name ${STACK_NAME} \
-  --template-body file://${DIR}/cron-batch-stack.yml \
-  --capabilities CAPABILITY_IAM \
-  --parameters file://${DIR}/parameters.json \
-  --tags file://${DIR}/tags.json 
+  --template-body main.yaml
 )
 
 
@@ -21,4 +21,4 @@ STACK_ID=$( \
 echo "Waiting on ${STACK_ID} create completion..."
 aws cloudformation wait stack-create-complete --stack-name ${STACK_ID}
 aws cloudformation describe-stacks --stack-name ${STACK_ID} 
-aws cloudformation list-stack-resources --stack-name myteststack
+aws cloudformation list-stack-resources --stack-name myriadbaseinfra
