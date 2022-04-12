aacdbe26c57d8a7d1dc70584da15f36947088714
sgraciamyriad <sgracia@myriad.com>
2022-04-12 10:42:44 -0500

    clean up entire project/test files

--- gocdagent.sh
+++
@@ -1,7 +0,0 @@
-echo "CONFIGURING GOCD AGENT access..."
-
-aws cloudformation create-stack /
---stack-name <stack_name> /
---region <region_name> /
---capabilities CAPABILITY_IAM /
---template-body file:///<path_to_json_template_file>
